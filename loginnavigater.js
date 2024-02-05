b67ad5a3b388a808c82b1688a61c4611c818573c
David Wilke <david-wilke@live.com>
2024-02-05 22:13:50 +0100

    seperate js function in single files

--- loginnavigater.js
+++ loginnavigater.js
@@ -1,12 +1,22 @@
-
 document.getElementById('user').addEventListener("click",goUser);
 document.getElementById('guest').addEventListener("click",goGuest);
+
+
+function checkForaPp(){
+    let appbox = document.querySelector('#app');
+    if (appbox.checked){
+        document.getElementById('signUpButton').disabled = false;
+    }
+    else{
+        console.log('Please confirm the Privacy policy.');
+        alert('Please confirm the Privacy policy.');
+    }
+}
 
 
 function checkForAccess(){
     let mail = document.getElementById('mail');
     let pass = document.getElementById('pass');
-    // debugger;
     console.log(mail.value);
     console.log(pass.value);
 }
@@ -19,4 +29,8 @@ function goGuest(){
 function goUser(){
     console.log("es wurde der Login Button angeklickt");
     checkForAccess();
+}
+
+function goSignUp(){
+    window.location.href = './index.html';
 }
