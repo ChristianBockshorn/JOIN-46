2be872e97378432cafbc9e78ae268c76eb7a7a60
AChlouh <anouar.ch@web.de>
2024-02-09 16:13:19 +0100

    AddTask-Dialog

---
+++ js/board.js
@@ -0,0 +1,8 @@
+function openDialog(text) {
+    document.getElementById('dialog').classList.remove('d-none');
+    document.getElementById('dialog-message').innerHTML = '';
+}
+
+function closeDialog() { 
+    document.getElementById('dialog').classList.add('d-none');
+}
