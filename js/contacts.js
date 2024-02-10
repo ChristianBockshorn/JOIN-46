34d9e7384065a00deb0f3718db5de905fb0c9fcb
David Wilke <david-wilke@live.com>
2024-02-10 16:03:18 +0100

    add display toggle function and optimize js code

--- js/contacts.js
+++ js/contacts.js
@@ -1,21 +1,22 @@
 let contacts = [];
+let currentContact;
 
-
-async function getData(){
+async function getData() {
   let all = await getItem('Contacts');
   contacts = all['data']['value'];
-  contacts = contacts.sort();
-  console.log(contacts);
+  // contacts = contacts.sort(); /*Sortiefunktion muss überarbeitet werden geht so nicht*/
+  // console.log(contacts);
   return contacts;
 }
 
 
 async function renderContacts() {
+  await getData();
   let con_content = document.getElementById('contact-content');
   con_content.innerHTML = '';
-  let all = await getItem('Contacts');
-  contacts = all['data']['value'];
-  console.log(contacts);
+  // let all = await getItem('Contacts');
+  // contacts = all['data']['value'];
+  // console.log(contacts);
   // contacts = getData();
   // debugger;
   j = 0;
@@ -26,20 +27,20 @@ async function renderContacts() {
       con_content.innerHTML += template_letterFrame(letter);
       letterold = letter;
       j++
-    }else{
+    } else {
       j = 0;
     }
-    console.log(letter);
+    // console.log(letter);
     let name = contacts[i]['name'];
     let mail = contacts[i]['email'];
     let path = contacts[i]['imgpath'];
-    console.log(path);
-    con_content.innerHTML += template_NameFrame(name, mail,path);
+    // console.log(path);
+    con_content.innerHTML += template_NameFrame(i, name, mail, path);
   }
 }
 
 
-function template_letterFrame(letter){
+function template_letterFrame(letter) {
   return `
           <div class="letter-container d-flex fd-column ai-start gap-8">
               <div class="letter">${letter}</div>
@@ -49,9 +50,35 @@ function template_letterFrame(letter){
 }
 
 
-function template_NameFrame(name, mail, path){
+document.addEventListener('DOMContentLoaded', function () {
+  renderContacts();
+});
+
+function generateDetails(i){
+  checkDisplayNone(i);
+  let name = contacts[i]['name'];
+  let mail = contacts[i]['email'];
+  let path = contacts[i]['imgpath'];
+  let phone = contacts[i]['telephone'];
+  let detailFrame = document.getElementById('main-content');
+  detailFrame.innerHTML = template_ContactDetails(name, path, mail, phone);
+}
+
+function checkDisplayNone(i){
+  if(currentContact == i){
+    document.getElementById('main-content').style.display = 'none'
+    currentContact = '-1';
+  }
+  else{
+    document.getElementById('main-content').style.display = 'flex'
+    currentContact = i;
+  }
+}
+
+
+function template_NameFrame(i, name, mail, path) {
   return `
-          <div class="content-container d-flex ai-center gap-35">
+          <div class="content-container d-flex ai-center gap-35" id="${i}" onclick="generateDetails(${i})">
               <img src="${path}">
               <div class="frame d-flex fd-column gap-5">
                 <div>${name}</div>
@@ -62,6 +89,31 @@ function template_NameFrame(name, mail, path){
 };
 
 
-document.addEventListener('DOMContentLoaded', function() {
-  renderContacts();
-});
+function template_ContactDetails(name, path, mail, phone){
+return `<div class="d-flex ai-center gap-54">
+        <img src="${path}" alt="Icon mit Initialen">
+          <div class="d-flex fd-column gap-8">
+            <h5>${name}</h5>
+            <div class="options d-flex gap-12">
+              <div class="d-flex gap-8">
+                <img src="/assets/images/edit_white.svg" alt="">Edit
+              </div>
+              <div class="d-flex gap-8">
+                <img src="/assets/images/delete.svg" alt="">Delete
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="height-74 d-flex ai-center">Contact Information</div>
+        <div class="f-weight-700 d-flex fd-column gap-22">
+          <div class="d-flex fd-column gap-15">
+            Email
+            <a href="mailto:${mail}">${mail}</a>
+          </div>
+          <div class="d-flex fd-column gap-15">
+            Phone
+            <a href="tel:${phone}">${phone}</a>
+          </div>
+        </div>
+      `;
+}
