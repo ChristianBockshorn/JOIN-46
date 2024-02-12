78cf48fed45ce22dddaeb2e5b33d5aee93a6a0c1
Christian Bockshorn <bockshorn@gmx.net>
2024-02-12 19:53:10 +0100

    add function addTask()

--- add_Task.js
+++ add_Task.js
@@ -1,3 +1,6 @@
+let AllTask = [];
+
+
 async function includeHTML() {
     let includeElements = document.querySelectorAll('[w3-include-html]');
     for (let i = 0; i < includeElements.length; i++) {
@@ -10,9 +13,45 @@ async function includeHTML() {
             element.innerHTML = 'Page not found';
         }
     }
+
 }
 
 function myFunction() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
+}
+
+function addTask() {
+    let title = document.getElementById('title').value;
+    let description = document.getElementById('description').value;
+    let assigned = document.getElementById('assigned').value;
+    let date = document.getElementById('date').value;
+
+    let urgent = document.getElementById('Priourgent').value;
+    let medium = document.getElementById('Priomedium').value;
+    let low = document.getElementById('Priolow').value;
+
+
+
+    let Tasks = {
+        "title": title,
+        "Description": description,
+        "Assigned-to": assigned,
+        "Due-date": date,
+        "Prio": urgent,medium,low,
+        "Category": "Technical Task",
+        "Subtasks": {}
+    };
+
+    AllTask.push(Tasks);
+
+    let AllTaskAsString=JSON.stringify(AllTask);
+    localStorage.setItem('AllTask',AllTaskAsString);
+
+
+
+
+
+
+
 }
