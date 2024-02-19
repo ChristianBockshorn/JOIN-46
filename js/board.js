bf9843dd6dd6a33a202ef5513e38174ca82f5968
Christian Bockshorn <bockshorn@gmx.net>
2024-02-19 20:30:50 +0100

    edit add task

--- js/board.js
+++ js/board.js
@@ -4,15 +4,38 @@ function openDialog(text) {
 
 }
 
-function closeDialog() { 
+
+function closeDialog() {
     document.getElementById('dialog').classList.add('d-none');
 }
+
 
 function doNotClose(event) {
     event.stopPropagation();
 }
 
+function loadAllTasks() {
+    let AllTaskAsString = localStorage.getItem('AllTask');
+    AllTask = JSON.parse(AllTaskAsString);
+    console.log('loaded task', AllTask);
 
+    render();
+}
+
+function render() {
+    console.log('test')
+    let content = document.getElementById("borderBoard");
+    content.innerHTML = '';
+    content.innerHTML += `
+    <h4>${AllTask[0].title}</h4>
+    <span>Description: ${AllTask[0].Description}</span>
+    <span>Assigned to: ${AllTask[0].Assigned}</span>
+    <span>Date: ${AllTask[0].date}</span>
+    <span>Prio: ${AllTask[0].Prio}</span>
+    <span>Category: ${AllTask[0].Category}</span>
+    Subtasks: [],
+    `;
+}
 
 
 
