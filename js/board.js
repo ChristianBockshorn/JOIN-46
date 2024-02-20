c13644dddd45b525145c1cf6c232a8c5c8be9e88
AChlouh <anouar.ch@web.de>
2024-02-20 11:59:46 +0100

    Board

--- js/board.js
+++ js/board.js
@@ -13,6 +13,63 @@ function doNotClose(event) {
 }
 
 
+let todos = [{
+    'id': 0,
+    'title': 'Putzen',
+    'category': 'open'
+}, {
+    'id': 1,
+    'title': 'Kochen',
+    'category': 'open'
+}, {
+    'id': 2,
+    'title': 'Einkaufen',
+    'category': 'closed'
+}];
 
+let currentDraggedElement;
 
+function updateHTML() {
+    let open = todos.filter(t => t['category'] == 'open');
 
+    document.getElementById('open').innerHTML = '';
+
+    for (let index = 0; index < open.length; index++) {
+        const element = open[index];
+        document.getElementById('open').innerHTML += generateTodoHTML(element);
+    }
+
+    let closed = todos.filter(t => t['category'] == 'closed');
+
+    document.getElementById('closed').innerHTML = '';
+
+    for (let index = 0; index < closed.length; index++) {
+        const element = closed[index];
+        document.getElementById('closed').innerHTML += generateTodoHTML(element);
+    }
+}
+
+function startDragging(id) {
+    currentDraggedElement = id;
+}
+
+function generateTodoHTML(element) {
+    return `<div draggable="true" ondragstart="startDragging(${element['id']})" class="todo">${element['title']}</div>`;
+}
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function moveTo(category) {
+    todos[currentDraggedElement]['category'] = category;
+    updateHTML();
+}
+
+function highlight(id) {
+    document.getElementById(id).classList.add('drag-area-highlight');
+}
+
+function removeHighlight(id) {
+    document.getElementById(id).classList.remove('drag-area-highlight');
+}
