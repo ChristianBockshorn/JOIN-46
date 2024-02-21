ea696ce565876dafe93e9eeabfffdc49d4218255
Christian Bockshorn <bockshorn@gmx.net>
2024-02-21 10:32:34 +0100

    edit render function

--- js/board.js
+++ js/board.js
@@ -14,6 +14,7 @@ function doNotClose(event) {
     event.stopPropagation();
 }
 
+
 function loadAllTasks() {
     let AllTaskAsString = localStorage.getItem('AllTask');
     AllTask = JSON.parse(AllTaskAsString);
@@ -22,20 +23,25 @@ function loadAllTasks() {
     render();
 }
 
+
 function render() {
-    console.log('test')
     let content = document.getElementById("borderBoard");
     content.innerHTML = '';
-    content.innerHTML += /* html */`
-    <span class="taskCategory">${AllTask[0].Category}</span>
-    <h3 class="taskTitle">${AllTask[0].title}</h3>
-    <span class="taskDescription">${AllTask[0].Description}</span>
-    <span>Assigned to: ${AllTask[0].Assigned}</span>
-    <span>Date: ${AllTask[0].date}</span>
-    <span>Prio: ${AllTask[0].Prio}</span>
-    
-    Subtasks: [],
-    `;
+    for (let i = 0; i < AllTask.length; i++) {
+        const task = AllTask[i];
+        
+        content.innerHTML += /* html */`
+        <div class="borderBoard">
+            <span class="taskCategory">${task.Category}</span>
+            <h3 class="taskTitle">${task.title}</h3>
+            <span class="taskDescription">${task.Description}</span>
+            <span>Assigned to: ${task.Assigned}</span>
+            <span>Date: ${task.date}</span>
+            <span>Prio: ${task.Prio}</span>
+            Subtasks: [],
+        </div>
+        `;
+    }
 }
 
 
