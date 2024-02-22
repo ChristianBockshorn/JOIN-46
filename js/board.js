9fe1d8811fe1f3d3a3b8aaf7bc5b9963c95bfc2c
Christian Bockshorn <bockshorn@gmx.net>
2024-02-22 17:09:30 +0100

    edit for taskDetail

--- js/board.js
+++ js/board.js
@@ -1,7 +1,6 @@
-function openDialog(text) {
+function openDialog() {
     document.getElementById('dialog').classList.remove('d-none');
     document.getElementById('mainContent').classList.remove('main');
-
 }
 
 
@@ -10,14 +9,24 @@ function closeDialog() {
 }
 
 
+
+
 function doNotClose(event) {
     event.stopPropagation();
 }
 
 
+function openTask() {
+    document.getElementById('taskDetail').classList.remove('d-none');
+    // document.getElementById('taskContent').classList.remove('main');
+}
+
+
 function render() {
-    let content = document.getElementById("borderBoard");
+    let content = document.getElementById('borderBoard');
     let noToDoDiv = document.querySelector('.no-to-do');
+    let taskContent = document.getElementById('taskDetail');
+
     content.innerHTML = '';
 
     if (AllTask.length > 0) {
@@ -25,7 +34,7 @@ function render() {
             const task = AllTask[i];
 
             content.innerHTML += /*html*/`
-            <div class="borderBoard">
+            <div class="borderBoard" onclick="openTask()">
                 <span class="taskCategory">${task.Category}</span>
                 <h3 class="taskTitle">${task.title}</h3>
                 <span class="taskDescription">${task.Description}</span>
@@ -36,10 +45,24 @@ function render() {
             </div>
             `;
         }
-        
+
         noToDoDiv.style.display = 'none';
     } else {
         noToDoDiv.style.display = 'block';
     }
+
+    taskContent = '';
+    taskContent += taskContentHtML();
+
+}
+
+
+function taskContentHtML(){
+    return /*html*/`
+    <h1>test</h1>
+    
+    
+    
+    `;
 }
 
