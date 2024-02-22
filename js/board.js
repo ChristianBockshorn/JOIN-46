e6208728ed7589e7438f5d3e36e86aac1241e196
Christian Bockshorn <bockshorn@gmx.net>
2024-02-22 13:41:03 +0100

    edit addTask function

--- js/board.js
+++ js/board.js
@@ -17,26 +17,29 @@ function doNotClose(event) {
 
 function render() {
     let content = document.getElementById("borderBoard");
+    let noToDoDiv = document.querySelector('.no-to-do');
     content.innerHTML = '';
-    for (let i = 0; i < AllTask.length; i++) {
-        const task = AllTask[i];
 
-        content.innerHTML += /*html*/`
-        <div class="borderBoard">
-            <span class="taskCategory">${task.Category}</span>
-            <h3 class="taskTitle">${task.title}</h3>
-            <span class="taskDescription">${task.Description}</span>
-            <span>Assigned to: ${task.Assigned}</span>
-            <span>Date: ${task.date}</span>
-            <span>Prio: ${task.Prio}</span>
-            Subtasks: [],
-        </div>
-        `;
+    if (AllTask.length > 0) {
+        for (let i = 0; i < AllTask.length; i++) {
+            const task = AllTask[i];
+
+            content.innerHTML += /*html*/`
+            <div class="borderBoard">
+                <span class="taskCategory">${task.Category}</span>
+                <h3 class="taskTitle">${task.title}</h3>
+                <span class="taskDescription">${task.Description}</span>
+                <span>Assigned to: ${task.Assigned}</span>
+                <span>Date: ${task.date}</span>
+                <span>Prio: ${task.Prio}</span>
+                Subtasks: [],
+            </div>
+            `;
+        }
+        
+        noToDoDiv.style.display = 'none';
+    } else {
+        noToDoDiv.style.display = 'block';
     }
-
 }
-
-
-
-
 
