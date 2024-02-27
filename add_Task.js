acb578f602f0d5651effed80a13676bc6316432b
David Wilke <david-wilke@live.com>
2024-02-27 21:18:56 +0100

    opimize assigned persons js

--- add_Task.js
+++ add_Task.js
@@ -1,5 +1,7 @@
 let AllTask = [];
 
+let assignedPersons = [];
+
 async function includeHTML() {
     let includeElements = document.querySelectorAll('[w3-include-html]');
     for (let i = 0; i < includeElements.length; i++) {
@@ -115,30 +117,73 @@ function closeDDListWithOutsideClick() {
 }
 
 
-function addToSelectedPersons(circle,i){
-    let selected = document.getElementById('selected-persons');
-    selected.innerHTML += `<img src="${circle}" id="${i}">`;
-    console.log(`<img src="${circle}" id="${i}">`);
+function deleteAssignedPerson(i) {
+    let toPurge =  assignedPersons.indexOf(i);
+    assignedPersons.splice(toPurge, 1);
 }
 
 
+function addAssignedPerson(i) {
+    assignedPersons.push(i);
+}
+
+
+function addToSelectedPersons(i) {
+    // debugger;
+    // let currentBox = document.getElementById(`checkbox${i}`).checked;
+    // if (currentBox == true) {
+        if (assignedPersons.indexOf(i) == -1) {
+            console.log('ist noch nicht vorhanden');
+            addAssignedPerson(i);
+            console.log(assignedPersons);
+        }
+        else if (assignedPersons.indexOf(i) > -1) {
+            console.log('ist bereits vorhanden');
+            deleteAssignedPerson(i);
+            console.log(assignedPersons);
+        // }
+    }
+
+    // selected persons in eine Variable speichern und dann wieder abfragen um die ausgewählten personen anzuzeigen wenn das assigned feld geschlossen wurde
+
+    let selected = document.getElementById('selected-persons');
+    selected.innerHTML = '';
+    for (let j = 0; j < assignedPersons.length; j++) {
+        selected.innerHTML += `<img src="${contacts[j]['imgpath']}" id="${assignedPersons[j]}">`;
+        console.log(`<img src="${contacts[j]['imgpath']}" id="checkbox${i}">`);
+    }
+}
+
 async function renderDropDownList() {
-    await getData();
+    // debugger;
     let ddfield = document.getElementById('dd-list-content');
     ddfield.innerHTML = '';
+    await getData();
     for (let i = 0; i < contacts.length; i++) {
         // console.log('txt');
-        let circle = contacts[i]['imgpath'];
-        // console.log(circle);
+        let initialsCircle = contacts[i]['imgpath'];
+        // console.log(initialsCircle);
         let name = contacts[i]['name'];
-        ddfield.innerHTML += `
+        if (assignedPersons.find(element => element == i) == i) {
+            ddfield.innerHTML += `
         <div id="dd-line" class="dd-line">
             <div class="dd-line-inline">
-                <img src="${circle}"></img>
+                <img src="${initialsCircle}"></img>
                 ${name}
             </div>
-            <input onclick="addToSelectedPersons('${circle}','${i}')" class="assigned-cbox" id="checkbox${i}" type="checkbox"></input>
+            <input onclick="addToSelectedPersons('${i}')" class="assigned-cbox" id="checkbox${i}" type="checkbox" checked></input>
         </div>`;
+        }
+        else {
+            ddfield.innerHTML += `
+            <div id="dd-line" class="dd-line">
+                <div class="dd-line-inline">
+                    <img src="${initialsCircle}"></img>
+                    ${name}
+                </div>
+                <input onclick="addToSelectedPersons('${i}')" class="assigned-cbox" id="checkbox${i}" type="checkbox" ></input>
+            </div>`;
+        }
     }
 }
 
