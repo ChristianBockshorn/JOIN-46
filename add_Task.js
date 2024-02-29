be769968524986fb0ae86088b539987eb2105739
David Wilke <david-wilke@live.com>
2024-02-29 19:21:56 +0100

    move html-template js function to storage.js

--- add_Task.js
+++ add_Task.js
@@ -2,20 +2,6 @@ let AllTask = [];
 
 let assignedPersons = [];
 
-async function includeHTML() {
-    let includeElements = document.querySelectorAll('[w3-include-html]');
-    for (let i = 0; i < includeElements.length; i++) {
-        const element = includeElements[i];
-        file = element.getAttribute("w3-include-html"); // "includes/header.html"
-        let resp = await fetch(file);
-        if (resp.ok) {
-            element.innerHTML = await resp.text();
-        } else {
-            element.innerHTML = 'Page not found';
-        }
-    }
-}
-
 
 function myFunction() {
     var popup = document.getElementById("myPopup");
--- js/storage.js
+++ js/storage.js
@@ -2,6 +2,21 @@ const STORAGE_TOKEN = '6YNH2EX6ZG2XYC0BJS4R41M1AVCZ3HI73LYQR1BW';
 const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
 
 
+async function includeHTML() {
+    let includeElements = document.querySelectorAll('[w3-include-html]');
+    for (let i = 0; i < includeElements.length; i++) {
+        const element = includeElements[i];
+        file = element.getAttribute("w3-include-html"); // "includes/header.html"
+        let resp = await fetch(file);
+        if (resp.ok) {
+            element.innerHTML = await resp.text();
+        } else {
+            element.innerHTML = 'Page not found';
+        }
+    }
+}
+
+
 let currentContact;
 let loggedinuser = [];
 
